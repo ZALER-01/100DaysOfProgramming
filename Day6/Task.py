403f55cbdced2be6739b6e576c7e4912edafd4c5
Choudhary <Rituraj.Choudhary@dieboldnixdorf.com>
2026-08-06 22:42:04 +0530

    Day 6 Started

---
+++ Day6/Task.py
@@ -0,0 +1,12 @@
+print(f'Hello, World!')
+
+num_char = len("Hello, World!")
+print(num_char)
+
+def my_function():
+    x = int(input("Enter a number: "))
+    y = int(input("Enter another number: "))
+    return x + y
+print(my_function())
+
+
