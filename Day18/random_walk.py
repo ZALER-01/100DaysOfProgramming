8300a7bbedf334f32df86d59828301bed40495b8
Choudhary <Rituraj.Choudhary@dieboldnixdorf.com>
2026-08-24 23:10:28 +0530

    comminting spirograph

--- Day18/random_walk.py
+++ Day18/random_walk.py
@@ -1,10 +1,25 @@
 import random
 import turtle as t
 
+t.colormode(255)
+
 tim = t.Turtle()
+tim.pensize(14)
+tim.speed("fastest")
 
-colors = ["red", "orange", "yellow", "green", "blue", "pink", "purple"]
-directions = [0,90,180,270]
+directions = [0, 90, 180, 270]
 
-tim.forward(30)
-tim.setheading(90)
+def random_color():
+    return (
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255)
+    )
+
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(random.randint(10, 30))
+    tim.setheading(random.choice(directions))
+
+screen = t.Screen()
+screen.exitonclick()
