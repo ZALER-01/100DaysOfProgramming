05911caac31026c281b0e06622a284020239c0cc
Choudhary <Rituraj.Choudhary@dieboldnixdorf.com>
2026-08-24 13:07:17 +0530

    Testing

---
+++ Day18/random_walk.py
@@ -0,0 +1,10 @@
+import random
+import turtle as t
+
+tim = t.Turtle()
+
+colors = ["red", "orange", "yellow", "green", "blue", "pink", "purple"]
+directions = [0,90,180,270]
+
+tim.forward(30)
+tim.setheading(90)
